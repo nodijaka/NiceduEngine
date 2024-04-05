0561b5a7f7d44a3cbe19424271ecd1970797b798
carlitzer <cjgribel@gmail.com>
2024-04-05 22:11:12 +0200

    cmake imgui-sfml + GUI test

--- main.cpp
+++ main.cpp
@@ -1,36 +1,39 @@
-/*#include <iostream>
-
-int main(int, char**){
-    std::cout << "Hello, from project!\n";
-    
-}
-*/
-
 #include <SFML/Graphics.hpp>
+#include <SFML/OpenGL.hpp>
+#include <imgui.h>
+#include <imgui-SFML.h>
 
 int main() {
-    
-    // Create a window with dimensions 800x600
-    //sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Window");
-    sf::Window window(sf::VideoMode(sf::Vector2u(800, 600)), "SFML Window");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML + ImGui");
 
-    // Main loop that runs until the window is closed
+    // Setup ImGui binding
+    ImGui::SFML::Init(window);
+
+    // Main loop
     while (window.isOpen()) {
-        // Process events
         sf::Event event;
         while (window.pollEvent(event)) {
+            ImGui::SFML::ProcessEvent(event);
+
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
-        // Clear the window with a black color
-        //window.clear(sf::Color::Black);
+        // Start the ImGui frame
+        ImGui::SFML::Update(window, sf::seconds(1.f / 60.f));
 
-        // Display the contents of the window
+        // ImGui demo window
+        ImGui::ShowDemoWindow();
+
+        // Rendering
+        window.clear();
+        // Your SFML drawing goes here
+        ImGui::SFML::Render(window);
         window.display();
     }
 
+    // Cleanup
+    ImGui::SFML::Shutdown();
+
     return 0;
 }
-
-
